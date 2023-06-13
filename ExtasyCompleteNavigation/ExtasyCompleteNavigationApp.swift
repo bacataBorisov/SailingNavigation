1cf2909954a036eadc535b7203ecfaff7533712f
bacataBorisov <112765301+bacataBorisov@users.noreply.github.com>
2023-06-13 20:14:21 +0300

    Initial Commit

---
+++ ExtasyCompleteNavigation/ExtasyCompleteNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtasyCompleteNavigationApp.swift
+//  ExtasyCompleteNavigation
+//
+//  Created by Vasil Borisov on 13.06.23.
+//
+
+import SwiftUI
+
+@main
+struct ExtasyCompleteNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
